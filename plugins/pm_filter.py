29a9de492c1a7f149fef9b3f915bdc39de2b056c
AnonymousSri <108179660+AnonymousSri@users.noreply.github.com>
2022-12-02 10:58:58 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -711,7 +711,7 @@ async def auto_filter(client, msg, spoll=False):
             **locals()
         )
     else:
-        cap = f"පොතේ නම 👉: <code>{search}</code>\n\n\n😌 ඔයා හොයන පොත මෙතන නැත්තම් @Pusthalalaya11 ගෲප් එකට මැසේජ් එකක් දාන්න\n\nᴇx පොත Group එකේ: \n\nserch කරලා බලන්නdin"
+        cap = f"පොතේ නම 👉: <code>{search}</code>\n\n\n🙂 ඔයා හොයන පොත මෙතන නැත්තම් @Pusthalalaya11 ගෲප් එකට මැසේජ් එකක් දාන්න\n\nᴇx පොත Group එකේ: \n\nserch කරලා බලන්න"
     if imdb and imdb.get('poster'):
         try:
             hehe =  await message.reply_photo(photo=imdb.get('poster'), caption=cap[:1024],
